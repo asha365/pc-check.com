0e0bf3de1588789baa643ed8e9dc465aa6edddae
Asha <asha.engrbd@gmail.com>
2022-12-15 20:50:52 +0600

    added bootstrap css framework, install bootstrap and install react router then setup the react router and create many route, create customLink component and connected to the custom link and react router

---
+++ src/components/Header/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <Navbar bg="light" expand="lg">
+                <Container fluid>
+                <Navbar.Brand  href="#">
+                    <h4 style={{fontFamily: "Sofia,  sans-serif"}} className='logo-name'>PC CHECK</h4>
+                {/* <img src='/public/images/laptop-logo.jpg'></img> */}
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+
+                    <Nav className="navbar-link me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }}
+                    navbarScroll>
+
+                    <CustomLink to="/home">HOME</CustomLink>
+                    <CustomLink to="/reviews">REVIEWS</CustomLink>
+                    <CustomLink to="dashboard">DASHBOARD</CustomLink>
+                    <CustomLink to="blogs">BLOGS</CustomLink>
+                    <CustomLink to="about">ABOUT</CustomLink>
+
+                    </Nav>
+                </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+    }
+    
+
+export default Header;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+
+    return (
+        <div className='laptop-review-container'>
+            
+        </div>
+    );
+};
+
+export default Home;
